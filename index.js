8a72d3f1863da6dd006d512c1b0bfa2698aaa16b
BlueSetbyeol <setbyeol.c@gmail.com>
2024-10-15 12:19:13 +0200

    modif lié au .env

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import "dotenv/config";
 
 const log = console.log;
 log(
-	chalk.blue.bold(`${process.env.NAME1}`),
-	chalk.white.bgRed(`${process.env.NAME2}`),
-	chalk.yellow.italic(`${process.env.NAME3}`),
-	chalk.green.underline(`${process.env.NAME4}`),
+	chalk.blue.bold(process.env.NAME1),
+	chalk.white.bgRed(process.env.NAME2),
+	chalk.yellow.italic(process.env.NAME3),
+	chalk.green.underline(process.env.NAME4),
 );
