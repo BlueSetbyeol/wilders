1f51277f768b5dcd3728a000ec3c25483db175c9
BlueSetbyeol <setbyeol.c@gmail.com>
2024-10-15 11:44:13 +0200

    il a dit, c'est fini ma gueule

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ import chalk from "chalk";
 import "dotenv/config";
 
 const log = console.log;
-log(chalk.blue.bold(`${process.env.NAME1}`));
+log(
+	chalk.blue.bold(`${process.env.NAME1}`),
+	chalk.white.bgRed(`${process.env.NAME2}`),
+	chalk.yellow.italic(`${process.env.NAME3}`),
+	chalk.green.underline(`${process.env.NAME4}`),
+);
